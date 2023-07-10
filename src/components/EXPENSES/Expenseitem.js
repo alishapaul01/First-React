b15ee49438a997259eb39154e4440305ee92fa54
alishapaul01 <paulalisha32@gmail.com>
2023-07-10 18:10:14 +0530

    Added Button

--- src/components/EXPENSES/Expenseitem.js
+++ src/components/EXPENSES/Expenseitem.js
@@ -3,7 +3,12 @@ import './ExpenseDate';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem=(props)=>{
-
+    const clickHandler= ()=> {
+        console.log('Clicked!!!');
+    }
+    const deleteHandler=()=>{
+        console.log('Deleted');
+    }
     return ( 
     <div className='expense-item'>
         <ExpenseDate date={props.date}/>
@@ -11,6 +16,8 @@ const ExpenseItem=(props)=>{
         <h2>{props.title}</h2>
         <div className='expense-item__location'>{props.location}</div>
         <div className='expense-item__price'>Rs {props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
+        <button onClick={deleteHandler}>Delete Expense</button>
     </div>
        
     </div>
